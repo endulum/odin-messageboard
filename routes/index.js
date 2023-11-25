3741a18c453e6445b30fc15296b918757a1acb69
endulum <71853916+endulum@users.noreply.github.com>
2023-11-24 20:11:06 -0800

    removed need for /new router, made form functional

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'New Message' });
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.username,
+    added: new Date()
+  });
+
+  res.redirect('/');
+});
+
 module.exports = router;
