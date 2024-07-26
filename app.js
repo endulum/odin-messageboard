2a82f9384c12fa89a41d2de57f8ff1ce9937b0e2
endulum <71853916+endulum@users.noreply.github.com>
2024-07-26 21:10:38 +0100

    get rid of www directory and clean up app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// create app
+const express = require('express')
+const app = express()
 
-var indexRouter = require('./routes/index');
+// wire in pug
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-var app = express();
+// enable use of req.body
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// wire in packaged middleware
+const logger = require('morgan')
+app.use(logger('dev'))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+// wire in routers
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
+const createError = require('http-errors')
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -30,10 +30,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3000, () => console.log('Connected at port 3000'))
